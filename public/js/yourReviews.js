8b45d4b9fc7746835637b57c718ef15e145d6418
Basquiat <ememdaniel98@gmail.com>
2021-11-03 13:53:46 -0500

    got the text boxes to appear when update button is clicked

--- public/js/yourReviews.js
+++ public/js/yourReviews.js
@@ -1,33 +1,37 @@
 // add event listeners on update and delete reviews that link to correct CRUD routes
 
+// const { text } = require("express");
+
 const updateReviewEventHandler = async function(event) {
     event.preventDefault();
+    console.log(event.target.parentElement);
     console.log('clicked');
 
-    const textBox = document.createElement('textarea');
+    const textBox = document.createElement('input');
     const reviewsDiv = document.getElementById('reviewsDiv');
-    reviewsDiv.appendChild(textBox);
+    event.target.parentElement.appendChild(textBox);
     const reviewText = document.querySelector('#reviewText');
 
+    // const response = await fetch('/api/reviews', {
+    //     method: 'UPDATE',
+    //     body: JSON.stringify({
+    //         reviews: reviewText.value
+    //     }),
+    //     headers: { 'Content-Type': 'application/json' },
+    // });
 
-    const response = await fetch('/api/reviews', {
-        method: 'UPDATE',
-        body: JSON.stringify({
-            first_name: firstnameEl.value,
-            last_name: lastnameEl.value,
-            email: emailEl.value,
-            password: passwordEl.value,
-        }),
-        headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-        document.location.replace('/login');
-    } else {
-        alert('Failed to sign up');
-    }
+    //     if (response.ok) {
+    //         document.location.replace('/');
+    //     } else {
+    //         alert("didn't work bitch!");
+    //     }
 };
 
-document
-    .querySelector('#updateReview')
-    .addEventListener('click', updateReviewEventHandler);
+// const updateButtons = document.querySelectorAll('.updateReview')
+// for (let index = 0; index < updateButtons.length; index++) {
+//     const element = updateButtons[index];
+//     element.addEventListener('click', updateReviewEventHandler);
+// }
+document.querySelectorAll('.updateReview').forEach(element => {
+    element.addEventListener('click', updateReviewEventHandler);
+})
